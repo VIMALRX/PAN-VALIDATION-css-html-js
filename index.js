55bf7cabdae1113add253d06a92cd58a6270b683
VIMALRX <vimalkannan624@gmail.com>
2024-08-12 11:59:51 +0530

    changed

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 function check(){
     var c=document.getElementById("input").value;
-    var d=/^[A-Z]{3}[0-9]{4}$/
+    var d=/^[A-Z]{3}[0-9]{4}$/;
+    if(d.test(c)){
+        let v=document.getElementById("valid").textContent="valid";
+        valid.style.color="green";
+    }
+    else{
+        let s=document.getElementById("valid").textContent="Notvalid"; 
+        valid.style.color="red";
+    }
 }
